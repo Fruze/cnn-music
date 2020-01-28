a3f15b5ed8c61d6e6e36da8cae88c194d1bf0210
Andrea <lk.andrea@ymail.com>
2020-01-28 17:42:52 +0700

    Add prediction & dataset

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from keras.engine.saving import model_from_json
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
-from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing.image import ImageDataGenerator, os
 
 
 class CNN:
 
-    USE_EXISTING_MODEL = True
+    USE_EXISTING_MODEL = False
 
     def __init__(self):
         model = self.get_model(self.USE_EXISTING_MODEL)
-
-        # TODO: Detect
-        # TODO: Cut from music partiture
-        # print(model.predict(x_test[:4]))
+        # self.predict_data(model)
+        # TODO: Cut from music sheet
 
     def get_model(self, use_existing_model):
         print("Getting model..")
         if use_existing_model:
             return self.load_model()
 
-        data = self.load_data()
-        model = self.build_model(data)
+        train_data, validation_data = self.load_data()
+        model = self.build_model(train_data, validation_data)
 
         print("Getting model is Finished..")
         return model
@@ -36,6 +34,8 @@ class CNN:
         loaded_model = model_from_json(loaded_model_json)
         loaded_model.load_weights("model/model_weight.h5")
         print("Loading model is Finished..")
+
+        return loaded_model
 
     @staticmethod
     def save_model(model):
@@ -62,6 +62,16 @@ class CNN:
             target_size=(256, 64)
         )
 
+        validation_data = image_data_generator.flow_from_directory(
+            directory="data/valid/",
+            batch_size=1,
+            class_mode="categorical",
+            shuffle=True,
+            seed=42,
+            color_mode="grayscale",
+            target_size=(256, 64)
+        )
+
         # For showing data
         # x, y = train_data.next()
         # for i in range(0, 3):
@@ -75,22 +85,22 @@ class CNN:
         #     pyplot.show()
 
         print("Loading data is Finished..")
-        return train_data
+        return train_data, validation_data
 
-    def build_model(self, data):
+    def build_model(self, train_data, validation_data):
         print("Building model..")
         model = Sequential()
 
         model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(256, 64, 1)))
         model.add(Conv2D(32, kernel_size=3, activation='relu'))
         model.add(Flatten())
-        model.add(Dense(11, activation='softmax'))
-        model.compile(optimizer="Adam", loss="binary_crossentropy", metrics=["acc"])
+        model.add(Dense(13, activation='softmax'))
+        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
         model.fit_generator(
-            generator=data,
-            validation_data=data,
-            epochs=1
+            generator=train_data,
+            validation_data=validation_data,
+            epochs=50
         )
 
         # TODO: Evaluate
@@ -103,6 +113,31 @@ class CNN:
 
         print("Building model is Finished..")
         return model
+
+    @staticmethod
+    def predict_data(model):
+        image_data_generator = ImageDataGenerator()
+        test_data = image_data_generator.flow_from_directory(
+            directory="data/test/",
+            batch_size=1,
+            class_mode=None,
+            shuffle=False,
+            color_mode="grayscale",
+            target_size=(256, 64)
+        )
+
+        result = model.predict(test_data)
+        print(result)
+
+        classes = os.listdir("data/train")
+        if ".DS_Store" in classes:
+            classes.remove(".DS_Store")
+            classes = sorted(classes)
+
+        for val in result.argmax(axis=-1):
+            print(classes[val])
+
+        print([map(int, classes[x]) for x in result.argmax(axis=-1)])
 
 
 if __name__ == "__main__":
